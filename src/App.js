f8bc78b4c0f3b5cf0b8bb011f35c6678415c25d6
roluochKe <rasholuoch@gmail.com>
2021-11-24 20:04:49 +0300

    adding dashboard'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css'
 import DefaultLayout from './layout/Default.layout'
+import Dashboard from './pages/dashboard/Dashboard.page'
 
 // import Entry from './pages/entry/Entry.page'
 
@@ -8,7 +9,9 @@ const App = () => {
   return (
     <div className='app'>
       {/* <Entry /> */}
-      <DefaultLayout />
+      <DefaultLayout>
+        <Dashboard />
+      </DefaultLayout>
     </div>
   )
 }
