c2c380006fa3c6d6c66f07d55f51b5d5ed49337a
roluochKe <rasholuoch@gmail.com>
2021-11-26 14:10:43 +0300

    user login

--- src/pages/entry/Entry.page.js
+++ src/pages/entry/Entry.page.js
@@ -6,70 +6,28 @@ import PasswordReset from '../../components/password-reset/PasswordReset.compone
 import './entry.style.css'
 
 const Entry = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
   const [formLoad, setFormLoad] = useState('login')
-
-  const handleOnChange = (e) => {
-    const { name, value } = e.target
-    switch (name) {
-      case 'email':
-        setEmail(value)
-        break
-      case 'password':
-        setPassword(value)
-        break
-      default:
-        break
-    }
-  }
-
-  const handleOnSubmit = (e) => {
-    e.preventDefault()
-
-    if (!email || !password) {
-      return alert('Fill up all the required fields!')
-    }
-
-    // TODO Call api to submit form
-    console.log(email, password)
-  }
 
   const handleOnResetSubmit = (e) => {
     e.preventDefault()
-
-    if (!email) {
-      return alert('Fill up the required field!')
-    }
-
-    // TODO Call api to submit form
-    console.log(email)
   }
 
-  const formSwitcher = (formType) => {
-    setFormLoad(formType)
+  const formSwitcher = (frmType) => {
+    setFormLoad(frmType)
   }
 
   return (
     <div className='entry-page bg-info'>
       <Card className='form-box'>
         <Card.Body>
-          {formLoad === 'login' && (
-            <LoginForm
-              handleOnChange={handleOnChange}
-              email={email}
-              password={password}
-              handleOnSubmit={handleOnSubmit}
-              formSwitcher={formSwitcher}
-            />
-          )}
+          {formLoad === 'login' && <LoginForm formSwitcher={formSwitcher} />}
 
-          {formLoad === 'reset' && (
+          {formLoad === 'rest' && (
             <PasswordReset
-              handleOnChange={handleOnChange}
-              email={email}
+              // handleOnChange={handleOnChange}
               handleOnResetSubmit={handleOnResetSubmit}
               formSwitcher={formSwitcher}
+              // email={email}
             />
           )}
         </Card.Body>
